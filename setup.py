8ea1556ff7946d6a4e114cdd0f06ab2b8ef8622d
Gabbeh <henrik.v.gabrielsen@gmail.com>
2018-07-01 21:57:39 +0200

    Setupfile
    added setupfile and changed mail-from to envvar.

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+with open('readme.md') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='wuphf',
+    version='0.0.2',
+    author='hvgab',
+    description='collection of clients to send messages with',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hvgab/wuphf',
+    packages=setuptools.find_packages())
--- wuphf/clients/sendgrid.py
+++ wuphf/clients/sendgrid.py
@@ -1,36 +1,39 @@
+import logging
+import os
 
 import sendgrid
 from sendgrid.helpers.mail import *
+
 from .. import wuphf
-import os
-import logging
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+
 class Sendgrid(wuphf.Client):
-	"""docstring for sendgrid_client."""
-	def __init__(self):
-		# super(sendgrid_client, self).__init__()
-		self.apikey = os.getenv('WUPHF_SENDGRID_API_TOKEN')
-		log.debug(f'apikey {self.apikey}')
-		self.sg = sendgrid.SendGridAPIClient(apikey=self.apikey)
+    """docstring for sendgrid_client."""
 
-	def format(self, message, send_to):
-		""" Builds a Sendgrid Mail object """
-		from_email = Email('WUPHF@drs.no')  # TODO: Get an env var for this
-		to_email = [Email(_) for _ in send_to]
-		subject = message.title
-		content = Content("text/plain", message.body)
-		mail = Mail(from_email, subject, to_email.pop(0), content)
-		for addr in to_email:
-			mail.personalizations[0].add_to(addr)
-		return mail.get()
+    def __init__(self):
+        # super(sendgrid_client, self).__init__()
+        self.apikey = os.getenv('WUPHF_SENDGRID_API_TOKEN')
+        log.debug(f'apikey {self.apikey}')
+        self.sg = sendgrid.SendGridAPIClient(apikey=self.apikey)
 
-	def send(self, message, send_to):
-		""" Sends the sendgrid mail object """
-		msg = self.format(message, send_to)
-		response = self.sg.client.mail.send.post(request_body=msg)
-		log.debug(response.status_code)
-		log.debug(response.body)
-		log.debug(response.headers)
+    def format(self, message, send_to):
+        """ Builds a Sendgrid Mail object """
+        from_email = Email(os.getenv('WUPHF_SENDGRID_FROM'))
+        to_email = [Email(_) for _ in send_to]
+        subject = message.title
+        content = Content("text/plain", message.body)
+        mail = Mail(from_email, subject, to_email.pop(0), content)
+        for addr in to_email:
+            mail.personalizations[0].add_to(addr)
+        return mail.get()
+
+    def send(self, message, send_to):
+        """ Sends the sendgrid mail object """
+        msg = self.format(message, send_to)
+        response = self.sg.client.mail.send.post(request_body=msg)
+        log.debug(response.status_code)
+        log.debug(response.body)
+        log.debug(response.headers)
