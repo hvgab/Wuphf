4c18d3716ebd1d43cb0b1fd25a2d2f7df6e33a6c
Gabbeh <henrik.v.gabrielsen@gmail.com>
2018-07-31 13:23:02 +0200

    what

--- wuphf/wuphf.py
+++ wuphf/wuphf.py
@@ -1,97 +1,103 @@
-
 import logging
+
 import wuphf
 
-logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
 
 
 class Contact(object):
-	""" Wuphf Contact """
-	def __init__(self,
-				name,
-				mail=None,
-				sms=None,
-				purecloud_channel=None,
-				slack_channel=None):
-		# super(Contact, self).__init__()
-		self.name = name
-		self.mail = mail
-		self.sms = sms
-		self.purecloud_channel = purecloud_channel
-		self.slack_channel = slack_channel
+    """ Wuphf Contact """
+
+    def __init__(self,
+                 name,
+                 mail=None,
+                 sms=None,
+                 purecloud_channel=None,
+                 slack_channel=None):
+        # super(Contact, self).__init__()
+        self.name = name
+        self.mail = mail
+        self.sms = sms
+        self.purecloud_channel = purecloud_channel
+        self.slack_channel = slack_channel
+
 
 class Message(object):
-	""" Message class
+    """ Message class
 
 		Create a message once and send it with any client.
 		Client should format this as it seems fit.
 	"""
-	def __init__(self, title, body, files=None, images=None):
-		self.title = title
-		self.body = body
-		self.files = files
-		self.images = images
+
+    def __init__(self, title, body, files=None, images=None):
+        self.title = title
+        self.body = body
+        self.files = files
+        self.images = images
 
 
 class Client(object):
-	""" Messaging client """
-	def __init__(self):
-		raise NotImplementedError('Subclass must define this func.')
+    """ Messaging client """
 
-	def send(self, message, send_to):
-		""" Send message """
-		log.debug('super send')
+    def __init__(self):
+        raise NotImplementedError('Subclass must define this func.')
 
-		if isinstance(send_to, str):
-			send_to = [send_to]
+    def send(self, message, send_to):
+        """ Send message """
+        log.debug('super send')
 
-		assert isinstance(send_to, list)
+        if isinstance(send_to, str):
+            send_to = [send_to]
 
-		return message, send_to
-		# raise NotImplementedError('Subclass must define this func.')
+        assert isinstance(send_to, list)
+
+        return message, send_to
+        # raise NotImplementedError('Subclass must define this func.')
+
 
 class Wuphf(object):
-	""" The wuphf class """
-	def __init__(self):
-		log.debug('start Wuphf init')
-		self.smtp = wuphf.clients.SMTP()
-		self.sendgrid = wuphf.clients.Sendgrid()
-		self.slack = wuphf.clients.Slack()
-		self.purecloud = wuphf.clients.Purecloud()
-		self.sms = wuphf.clients.SMS_never()
+    """ The wuphf class """
 
-	def smtp(self, message, send_to):
-		log.debug('start send smtp')
-		self.smtp.send(message, send_to )
-		log.debug('end send smtp')
+    def __init__(self):
+        log.debug('start Wuphf init')
+        self.smtp = wuphf.clients.SMTP()
+        self.sendgrid = wuphf.clients.Sendgrid()
+        self.slack = wuphf.clients.Slack()
+        self.purecloud = wuphf.clients.Purecloud()
+        self.sms = wuphf.clients.SMS_never()
 
-	def sendgrid(self, message, send_to):
-		log.debug('start send sendgrid')
-		self.sendgrid.send(message, send_to)
-		log.debug('end send sendgrid')
+    def smtp(self, message, send_to):
+        log.debug('start send smtp')
+        self.smtp.send(message, send_to)
+        log.debug('end send smtp')
 
-	def slack(self, message, send_to):
-		self.slack.send(message, send_to)
+    def sendgrid(self, message, send_to):
+        log.debug('start send sendgrid')
+        self.sendgrid.send(message, send_to)
+        log.debug('end send sendgrid')
 
-	def purecloud(self, message, send_to):
-		self.purecloud.send(message, send_to)
+    def slack(self, message, send_to):
+        self.slack.send(message, send_to)
 
-	def sms(self, message, send_to):
-		self.sms_never.send(message, send_to)
+    def purecloud(self, message, send_to):
+        self.purecloud.send(message, send_to)
 
-	def send(self, message, contacts):
-		""" Send to multiple channel at the same time.
+    def sms(self, message, send_to):
+        self.sms_never.send(message, send_to)
+
+    def send(self, message, contacts):
+        """ Send to multiple channel at the same time.
 
 			Args:
 				msg
 				send_to: wuphf.Contact
 		"""
 
-		# TODO: Formater tekst 1 gang hvis lista er for lang
+        # TODO: Formater tekst 1 gang hvis lista er for lang
 
-		for contact in contacts:
-			self.mail(message, contact.mail)
-			self.sms(message, contact.sms)
-			self.purecloud(message, contact.purecloud_channel)
-			self.slack(message, contact.slack_channel )
+        for contact in contacts:
+            self.mail(message, contact.mail)
+            self.sms(message, contact.sms)
+            self.purecloud(message, contact.purecloud_channel)
+            self.slack(message, contact.slack_channel)
